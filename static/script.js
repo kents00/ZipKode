e8d3b42ff3e0707551aa8b5ccaf13e3ed8947662
Kent Edoloverio <kentedoloverio25@gmail.com>
2025-04-07 22:43:14 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // Initialize DataTable with enhanced features
     $('#zipTable').DataTable({
-        paging: true,
-        lengthMenu: [15, 30, 50, 100],
-        searching: true,
-        ordering: false
+        responsive: true,
+        pageLength: 10,
+        dom: '<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>' +
+            '<"row"<"col-sm-12"tr>>' +
+            '<"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
+        language: {
+            search: "Search records:",
+            lengthMenu: "Show _MENU_ entries",
+        }
     });
+
+    // Add hover effects to table rows
+    $('#zipTable tbody').on('mouseenter', 'tr', function () {
+        $(this).addClass('highlight-row');
+    }).on('mouseleave', 'tr', function () {
+        $(this).removeClass('highlight-row');
+    });
+
+    // Smooth scroll to top
+    $('.btn').addClass('animate__animated animate__pulse');
+
+    // Add loading animation
+    $(window).on('load', function () {
+        $('.container').fadeIn('slow');
+    });
+
+    // Add tooltip to the donate button
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 function searchZipCode() {
